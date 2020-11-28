9e7fb88b4dc1999d9a5287c83292e0fff5b01523
criniguez <felizpc1@gmail.com>
2020-11-28 14:41:35 -0400

    fases extra: archivos types

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import { GET_ALL } from '../types/usersTypes';
 
 export const getAll = () => async (dispatch) => {
   const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
   dispatch({
-    type: 'get_users',
+    type: GET_ALL,
     payload: data,
   });
 };
