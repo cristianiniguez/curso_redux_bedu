abe5d2ec48036b2464d71dac32a8f786ba73405a
criniguez <felizpc1@gmail.com>
2020-11-28 14:49:16 -0400

    fases extra: try catch

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -2,9 +2,13 @@ import axios from 'axios';
 import { GET_ALL } from '../types/usersTypes';
 
 export const getAll = () => async (dispatch) => {
-  const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
-  dispatch({
-    type: GET_ALL,
-    payload: data,
-  });
+  try {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
+    dispatch({
+      type: GET_ALL,
+      payload: data,
+    });
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 };
