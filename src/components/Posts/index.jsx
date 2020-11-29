609a18f8338d5fa5831c21fdca85f17ecde32af0
criniguez <felizpc1@gmail.com>
2020-11-29 13:17:05 -0400

    compartir informacion: compartir reducer

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as usersActions from '../../actions/usersActions';
 
 class Posts extends Component {
+  componentDidMount() {
+    if (!this.props.users.length) {
+      this.props.getAll();
+    }
+  }
   render() {
-    return <div>{this.props.match.params.key}</div>;
+    console.log(this.props);
+    return (
+      <div>
+        <h1>Publicaciones de -nombre-</h1>
+        {this.props.match.params.key}
+      </div>
+    );
   }
 }
 
-export default Posts;
+const mapStateToProps = (reducers) => {
+  return reducers.usersReducer;
+};
+
+export default connect(mapStateToProps, usersActions)(Posts);
