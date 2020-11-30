7b9734183c2a0da67cc62405ee4f52967d107323
criniguez <felizpc1@gmail.com>
2020-11-29 20:34:08 -0400

    compartir informacion: inmutabilidad

--- src/actions/postsActions.js
+++ src/actions/postsActions.js
@@ -1,12 +1,25 @@
 import axios from 'axios';
 import { GET_BY_USER } from '../types/postsTypes';
+import * as usersTypes from '../types/usersTypes';
+
+const { GET_ALL: USERS_GET_ALL } = usersTypes;
 
 export const getByUser = (key) => async (dispatch, getState) => {
   const { users } = getState().usersReducer;
   const { posts } = getState().postsReducer;
   const userId = users[key].id;
   const { data } = await axios.get(`http://jsonplaceholder.typicode.com/posts?userId=${userId}`);
-  const updatedPosts = [...posts, ...data];
+  const updatedPosts = [...posts, data];
+  const postsKey = updatedPosts.length - 1;
+  const updatedUsers = [...users];
+  updatedUsers[key] = {
+    ...users[key],
+    postsKey,
+  };
+  dispatch({
+    type: USERS_GET_ALL,
+    payload: updatedUsers,
+  });
   dispatch({
     type: GET_BY_USER,
     payload: updatedPosts,
