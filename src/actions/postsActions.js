a82998a16e80812ee9ae5c678deb13c01acd9b6a
criniguez <felizpc1@gmail.com>
2020-11-29 20:11:42 -0400

    compartir informacion: evitar segundas busquedas

--- src/actions/postsActions.js
+++ src/actions/postsActions.js
@@ -1,31 +1,14 @@
 import axios from 'axios';
-import { ERROR, GET_ALL, LOADING } from '../types/postsTypes';
-
-export const getAll = () => async (dispatch) => {
-  dispatch({
-    type: LOADING,
-  });
-  try {
-    const { data } = await axios.get('http://jsonplaceholder.typicode.com/posts');
-    dispatch({
-      type: GET_ALL,
-      payload: data,
-    });
-  } catch (error) {
-    console.error('Error:', error.message);
-    dispatch({
-      type: ERROR,
-      payload: 'Algo salió mal. Intente más tarde.',
-    });
-  }
-};
+import { GET_BY_USER } from '../types/postsTypes';
 
 export const getByUser = (key) => async (dispatch, getState) => {
   const { users } = getState().usersReducer;
+  const { posts } = getState().postsReducer;
   const userId = users[key].id;
   const { data } = await axios.get(`http://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+  const updatedPosts = [...posts, ...data];
   dispatch({
-    type: GET_ALL,
-    payload: data,
+    type: GET_BY_USER,
+    payload: updatedPosts,
   });
 };
