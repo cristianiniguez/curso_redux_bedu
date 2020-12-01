c23ae26d2209293623c4aef4300b3bd84000593b
criniguez <felizpc1@gmail.com>
2020-12-01 18:38:33 -0400

    compartir informacion: evitar sobreescritura

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Spinner from '../general/Spinner';
+import Fatal from '../general/Fatal';
+
 import * as usersActions from '../../actions/usersActions';
 import * as postsActions from '../../actions/postsActions';
 
@@ -9,17 +12,48 @@ const { getByUser: postsGetByUser } = postsActions;
 
 class Posts extends Component {
   async componentDidMount() {
+    const {
+      usersGetAll,
+      postsGetByUser,
+      match: {
+        params: { key },
+      },
+    } = this.props;
     if (!this.props.usersReducer.users.length) {
-      await this.props.usersGetAll();
+      await usersGetAll();
     }
-    await this.props.postsGetByUser(this.props.match.params.key);
+    if (this.props.usersReducer.error) {
+      return;
+    }
+    if (!('postsKey' in this.props.usersReducer.users[key])) {
+      console.log('getting posts');
+      await postsGetByUser(this.props.match.params.key);
+    }
   }
+
+  putUser = () => {
+    const {
+      usersReducer,
+      match: {
+        params: { key },
+      },
+    } = this.props;
+    if (usersReducer.error) {
+      return <Fatal mensaje={usersReducer.error} />;
+    }
+    if (!usersReducer.users.length || usersReducer.loading) {
+      return <Spinner />;
+    }
+    const name = usersReducer.users[key].name;
+    return <h1>Publicaciones de {name}</h1>;
+  };
+
   render() {
     console.log(this.props);
     return (
       <div>
-        <h1>Publicaciones de -nombre-</h1>
         {this.props.match.params.key}
+        {this.putUser()}
       </div>
     );
   }
